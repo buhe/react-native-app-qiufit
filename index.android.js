430305ed2a48356962742d6ab253a694f7867e41
harebu <bugu1986@126.com>
2015-12-11 17:19:41 +0800

    UPDATE android main view

--- index.android.js
+++ index.android.js
@@ -3,8 +3,8 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-
 var React = require('react-native');
+var Reflux = require('reflux');
 var {
     AppRegistry,
     StyleSheet,
@@ -14,6 +14,7 @@ var {
     } = React;
 import ExNavigator from '@exponent/react-native-navigator';
 var Router = require('./views/router');
+import UserStore from './stores/UserStore';
 
 var renderFunc = function (props) {
   return (
@@ -21,17 +22,40 @@ var renderFunc = function (props) {
   );
 };
 
-var PrisonerFitness = React.createClass({
+var MainView = React.createClass({
   render: function () {
-    return (
-        <ExNavigator
-            initialRoute={Router.getWelcome()}
-            style={{ flex: 1 }}
-            //sceneStyle={{ paddingTop: 64 }}
-            //renderNavigationBar={renderFunc}
-            showNavigationBar={false}
-            />
-    );
+    return (<ExNavigator
+        initialRoute={Router.getTypeList()}
+        style={{ flex: 1 }}
+        //sceneStyle={{ paddingTop: 64 }}
+        //renderNavigationBar={renderFunc}
+        showNavigationBar={false}
+        />);
+  }
+});
+
+var WelcomeView = React.createClass({
+  render: function () {
+    return (<ExNavigator
+        initialRoute={Router.getWelcome()}
+        style={{ flex: 1 }}
+        //sceneStyle={{ paddingTop: 64 }}
+        //renderNavigationBar={renderFunc}
+        showNavigationBar={false}
+        />);
+  }
+});
+
+var PrisonerFitness = React.createClass({
+  mixins: [Reflux.connect(UserStore)],
+  render: function () {
+    var view;
+    if (this.state.user.username && this.state.verify) {
+      view = <MainView />;
+    } else {
+      view = <WelcomeView />;
+    }
+    return view;
   }
 });
 
