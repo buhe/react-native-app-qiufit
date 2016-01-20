55e4d92fe5ef90694b1f78f314bf5ba025650c2e
harebu <bugu1986@126.com>
2016-01-20 15:27:28 +0800

    搞定了 iOS web 登录 还差一点

--- fb/index.js
+++ fb/index.js
@@ -10,8 +10,50 @@ var {
     TouchableHighlight,
     Text
     } = React;
-
+import UserActionCreators from '../actions/UserActionCreators';
 var FBLoginManager = NativeModules.FBLoginManager;
+var RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
+
+function _getInfo(user, callback) {
+  var api = `https://graph.facebook.com/v2.3/${user.userId}?fields=name,email,gender&access_token=${user.token}`;
+
+  fetch(api)
+      .then((response) => response.json())
+      .then((responseData) => {
+        callback(responseData);
+      })
+      .done();
+}
+
+function _registerUser(profile) {
+  var user = {
+    username: profile.name,
+    gender: profile.gender,
+    //avatarUrl: profile.headimgurl,
+    openId: profile.id,
+    accessToken: profile.token,
+    type: 'fb'
+  };
+  UserActionCreators.registerUser(user);
+}
+
+function _login(data) {
+  _getInfo(data.credentials, function (profile) {
+    profile.token = data.credentials.token;
+    _registerUser(profile);
+  });
+}
+
+RCTDeviceEventEmitter.addListener(
+    FBLoginManager.Events["Login"],
+    _login
+);
+RCTDeviceEventEmitter.addListener(
+    FBLoginManager.Events["LoginFound"],
+    _login
+);
+//iOS handler
+
 class FB {
   /**
    * Auth login
@@ -20,20 +62,21 @@ class FB {
     var permissions = ['email', 'public_profile'];
     FBLoginManager.loginWithPermissions(permissions, function (e, data) {
       var result = e || data;
+      //iOS handler
       if (result.type === 'success' && result.profile) {
         try {
           result.profile = JSON.parse(result.profile)
           result.profile.token = result.token;
         } catch (err) {
           console.warn('Could not parse facebook profile: ', result.profile);
-          alert(err);
+        }
+        if (result.eventName === 'onLogin' || result.eventName === 'onLoginFound') {
+          callback(result.profile);
+        } else {
+
         }
       }
-      if (result.eventName === 'onLogin' || result.eventName === 'onLoginFound') {
-        callback(result.profile);
-      } else {
 
-      }
     });
   }
 
