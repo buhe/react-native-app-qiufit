efb8f5fd6c9f0c594e9b2c1ae8083aff940d9d0e
harebu <bugu1986@126.com>
2016-02-02 18:17:53 +0800

    ...

--- ios/PrisonerFitness/AppDelegate.m
+++ ios/PrisonerFitness/AppDelegate.m
@@ -40,7 +40,7 @@
 
 
 //  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-//  jsCodeLocation = [NSURL URLWithString:@"http://10.29.206.174:8081/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://10.29.206.142:8081/index.ios.bundle?platform=ios&dev=true"];
 //  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.170:8081/index.ios.bundle?platform=ios&dev=true"];
   /**
    * OPTION 2
